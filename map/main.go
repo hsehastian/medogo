9c1cd971019789d5bd8693e8a8db724838f9ff2b
Hery Sehastian <sehastian.hery@gmail.com>
2021-09-03 22:53:50 +0700

    day 6: print map example

--- map/main.go
+++ map/main.go
@@ -1,21 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// var colors map[string]string
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "0000ff",
+	}
 
-	colors := make(map[int]string)
+	printMap(colors)
+}
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// 	"blue":  "0000ff",
-	// }
-
-	colors[10] = "#ffffff"
-
-	delete(colors, 10)
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
